882dc26f5580e23d41206cb4e28d8faa4204a7fd
cgcctns <bhupendrasahu.cctns@gmail.com>
2022-11-23 11:33:42 +0530

    width changes

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { BsTwitter, BsInstagram , BsLinkedin } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => (
@@ -12,6 +12,9 @@ const SocialMedia = () => (
     </div>
     <div>
       <BsInstagram />
+    </div>
+    <div>
+      <BsLinkedin />
     </div>
   </div>
 );
